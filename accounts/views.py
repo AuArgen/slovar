54280cd6862266a4933f7c7a20d9bfc298f28c5c
AuArgen <argenazizbekuulu3@gmail.com>
2024-01-16 09:11:30 +0600

    first application

---
+++ accounts/views.py
@@ -0,0 +1,31 @@
+# accounts/views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Замените 'home' на имя вашей главной страницы
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # Замените 'home' на имя вашей главной страницы
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('login')  # Замените 'login' на имя вашей страницы входа
