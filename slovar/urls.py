e8461163a4066ef38ff6cdacf859dff2c7a59ff9
AuArgen <argenazizbekuulu3@gmail.com>
2024-01-19 16:11:12 +0600

    ...

--- slovar/urls.py
+++ slovar/urls.py
@@ -1,9 +1,17 @@
 # mysite/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings  # Import the settings module
+from django.conf.urls.static import static  # Import the static function
 
 urlpatterns = [
     path('admin2024/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('', include('home.urls')),  # Добавьте эту строку для главной страницы
+    path('', include('home.urls')),  # Add this line for the main page
 ]
+
+# Add static files serving during development
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
