8c5cacc42c9acea85efc48fc2c3c1490be45b865
AuArgen <argenazizbekuulu3@gmail.com>
2024-01-25 09:44:00 +0600

    ...

--- home/admin.py
+++ home/admin.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
 from .models import *
 
+
 class WordAdmin(admin.ModelAdmin):
-    list_display = ('word',)
+    list_display = ('word', 'user_id')
+
+
+class LearnedWordAdmin(admin.ModelAdmin):
+    list_display = ('word', 'user_id')
+
 
 class TextAdmin(admin.ModelAdmin):
-    list_display = ('word',)
+    list_display = ('word', 'user_id')
+
+
+class LearnedTextAdmin(admin.ModelAdmin):
+    list_display = ('word', 'user_id')
 
 
 admin.site.register(Word, WordAdmin)
+admin.site.register(LearnedWord, WordAdmin)
 admin.site.register(Text, TextAdmin)
+admin.site.register(LearnedText, TextAdmin)
--- home/models.py
+++ home/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from accounts.models import CustomUser
 
+
 class Word(models.Model):
     user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     word = models.CharField(max_length=100)
     translate = models.CharField(max_length=100)
     create = models.DateTimeField()
+
     class Meta:
         verbose_name = 'word'
         verbose_name_plural = 'Words'
@@ -19,9 +21,20 @@ class Text(models.Model):
     word = models.CharField(max_length=500)
     translate = models.CharField(max_length=500)
     create = models.DateTimeField()
+
     class Meta:
         verbose_name = 'text'
         verbose_name_plural = 'Texts'
 
     def __str__(self):
         return str(self.word)
+
+
+class LearnedWord(models.Model):
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    word = models.ForeignKey(Word, on_delete=models.CASCADE)
+
+
+class LearnedText(models.Model):
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    word = models.ForeignKey(Text, on_delete=models.CASCADE)
