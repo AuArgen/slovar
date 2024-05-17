ce296dbd894d38c49be92c302322a9b8071dac5e
AuArgen <argenazizbekuulu3@gmail.com>
2024-05-18 02:23:10 +0600

    model teachers

--- slovar/settings.py
+++ slovar/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'languages.apps.LanguagesConfig',
     'lessons.apps.LessonsConfig',
+    'teachers.apps.TeachersConfig',
 ]
 
 MIDDLEWARE = [
