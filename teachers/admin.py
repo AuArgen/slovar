7e7db991be8cfb3211e859f1b5680e9052fd7ba0
AuArgen <argenazizbekuulu3@gmail.com>
2024-05-18 02:30:18 +0600

    news

--- teachers/admin.py
+++ teachers/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from teachers.models import Teacher
+from teachers.models import Teacher, News
 
 
 # Register your models here.
@@ -9,3 +9,9 @@ class TeacherAdmin(admin.ModelAdmin):
     list_display = ('id', 'fio', 'email', 'language')
     search_fields = ('id', 'email', 'language')
     list_filter = ('language',)
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description')
+    search_fields = ('id', 'title', 'description')
+    list_filter = ('title',)
--- teachers/models.py
+++ teachers/models.py
@@ -22,3 +22,18 @@ class Teacher(models.Model):
 
     def __str__(self):
         return self.fio
+
+
+class News(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='news/')
+    information = RichTextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'News'
+        verbose_name_plural = 'News'
+
+    def __str__(self):
+        return self.title
